b7ae9f5a840cc708cf9c9ee260fc5a9603a1cc22
Andrei <an94dy@yahoo.com>
2015-03-24 13:34:57 +0000

    First commit

---
+++ src/comp2010/target/DynamicVariableFolding.java
@@ -0,0 +1,12 @@
+package comp2010.target;
+
+public class DynamicVariableFolding {
+	public int optimiseMe () {
+		int a = 651;
+		int b = a;
+		int c = a;
+		int d = 3 + 10;
+		return a + b;
+	}
+
+}
